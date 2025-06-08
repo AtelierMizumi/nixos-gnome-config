4b6b63cf6e472497e140f4a22e2a63f456d65eef
AtelierMizumi <thuanc177@gmail.com>
2025-06-08 14:04:04 +0700

    Replace Google DNS with Mullvad DNS in network configuration

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -4,8 +4,7 @@
     hostName = "${host}";
     networkmanager.enable = true;
     nameservers = [
-      "8.8.8.8"
-      "8.8.4.4"
+      "all.dns.mullvad.net"
       "1.1.1.1"
     ];
     firewall = {
